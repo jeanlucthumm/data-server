534caa4a0cf65a8ec68c98bd3039853d4e9f1401
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2020-08-22 18:08:59 -0400

    Maintenance file

---
+++ maintenance.py
@@ -0,0 +1,46 @@
+# This file is for random one-time shit I had to do with python
+import argparse
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+def add_user_to_unowned_tasks_and_task_names(update_task_names=False, update_tasks=False):
+    """
+    After adding authentication, all the previous tasks and task names were missing user ID.
+    This function adds the passed UID to all of them
+    """
+    parser = argparse.ArgumentParser(description='Claim unowned tasks with given uid')
+    parser.add_argument('--cred',
+                        help='path to credential json file to authenticate with Firestore',
+                        required=True)
+    parser.add_argument('--uid', help='user ID', required=True)
+    flags = parser.parse_args()
+
+    cred = credentials.Certificate(flags.cred)
+    firebase_admin.initialize_app(cred)
+
+    if update_task_names:
+        task_names_ref = firebase_admin.firestore.client().collection('DividerTaskNames')
+        for doc in task_names_ref.stream():
+            data = doc.to_dict()
+            if 'userId' in data:
+                print(f"Skipping {data['name']}")
+                continue
+            print(f"Updating {data['name']} with uid {flags.uid}")
+            doc.reference.update({"userId": flags.uid})
+
+    if update_tasks:
+        tasks_ref = firebase_admin.firestore.client().collection('DividerTasks')
+        for doc in tasks_ref.stream():
+            data = doc.to_dict()
+            if 'userId' in data:
+                print(f"Skipping ({data['name']}, {data['startTime']})")
+                continue
+            print(f"Updating ({data['name']}, {data['startTime']}) with uid {flags.uid}")
+            doc.reference.update({"userId": flags.uid})
+
+
+if __name__ == '__main__':
+    add_user_to_unowned_tasks_and_task_names(update_task_names=False, update_tasks=True)
