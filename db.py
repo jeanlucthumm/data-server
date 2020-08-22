ce32eca832d1a2823fe592015f8a1baded0f02e2
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2020-08-22 17:14:37 -0400

    Enforce naming convention for private stuff in db

--- db.py
+++ db.py
@@ -15,35 +15,14 @@ handler.setFormatter(formatter)
 log.addHandler(handler)
 
 
-def default_connection(db_path):
-    conn = sqlite3.connect(db_path)
+def get_all_tasks(db_path):
+    conn = _default_connection(db_path)
     c = conn.cursor()
-    if not default_tables_exist(c):
-        create_default_tables(c)
-    conn.row_factory = sqlite3.Row
-    return conn
+    c.execute('SELECT * FROM Tasks')
+    return c.fetchall()
 
 
-def create_default_tables(cursor):
-    cursor.execute('''
-        CREATE TABLE Tasks(
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            startTime INTEGER NOT NULL
-        )
-    ''')
-
-
-def default_tables_exist(cursor):
-    cursor.execute('''
-        SELECT count(name)
-        FROM sqlite_master
-        WHERE type='table' AND name='Tasks'
-    ''')
-    return cursor.fetchone()[0] == 1
-
-
-def get_last_start_time(cursor):
+def _get_last_start_time(cursor):
     cursor.execute('''
         SELECT max(startTime) FROM Tasks
     ''')
@@ -54,30 +33,51 @@ def get_last_start_time(cursor):
         return res[0]
 
 
-def add_task(cursor, name, start_time):
+def _add_task(cursor, name, start_time):
     log.info('Adding task: name(%s), startTime(%d)', name, start_time)
     cursor.execute('''
         INSERT INTO Tasks VALUES (NULL, ?, ?)
     ''', (name, start_time))
 
 
-def get_all_tasks(db_path):
-    conn = default_connection(db_path)
+def _default_connection(db_path):
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute('SELECT * FROM Tasks')
-    return c.fetchall()
+    if not _default_tables_exist(c):
+        _create_default_tables(c)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
-def update_db(tasks_ref, db_path):
-    conn = default_connection(db_path)
+def _create_default_tables(cursor):
+    cursor.execute('''
+        CREATE TABLE Tasks(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            startTime INTEGER NOT NULL
+        )
+    ''')
+
+
+def _default_tables_exist(cursor):
+    cursor.execute('''
+        SELECT count(name)
+        FROM sqlite_master
+        WHERE type='table' AND name='Tasks'
+    ''')
+    return cursor.fetchone()[0] == 1
+
+
+def _update_db(tasks_ref, db_path):
+    conn = _default_connection(db_path)
     c = conn.cursor()
-    last_time = get_last_start_time(c)
+    last_time = _get_last_start_time(c)
     count = 0
     for doc in tasks_ref.where('startTime', '>', last_time).stream():
-        add_task(c, doc.get('name'), doc.get('startTime'))
+        _add_task(c, doc.get('name'), doc.get('startTime'))
         count += 1
     conn.commit()
-    after_last_time = get_last_start_time(c)
+    after_last_time = _get_last_start_time(c)
     log.info('Added %d tasks', count)
     log.info('Prev last time: %d, current last time: %d',
              last_time, after_last_time)
@@ -86,5 +86,5 @@ def update_db(tasks_ref, db_path):
 if __name__ == '__main__':
     cred = credentials.Certificate(sys.argv[1])
     firebase_admin.initialize_app(cred)
-    update_db(firebase_admin.firestore.client().collection('DividerTasks'),
-              sys.argv[2])
+    _update_db(firebase_admin.firestore.client().collection('DividerTasks'),
+               sys.argv[2])
